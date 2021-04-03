6e0c898dbc2fcaf7f1651978c2eef3c5006b347f
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-03 23:12:15 +0530

    Added Document Ready Event if fn is passed

--- custom-jquery/bisuQuery.js
+++ custom-jquery/bisuQuery.js
@@ -1,5 +1,15 @@
 /* eslint-disable */
 
-const $ = () => {
+const $ = (...args) => {
+
+	if(typeof args[0] === 'function') {
+		// document ready fn
+		const readyFn = args[0]
+		document.addEventListener('DOMContentLoaded', readyFn)
+	} else if(typeof args[0] === 'string') {
+		// Element is a selector
+		const selector = args[0]
+		return document.querySelectorAll(selector)
+	}
 
 }
