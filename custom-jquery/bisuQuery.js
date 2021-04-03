8dff550bbc5853e5bee2c1b440e7401f8f430286
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-04 00:21:46 +0530

    Implemented basic simplified jquery

--- custom-jquery/bisuQuery.js
+++ custom-jquery/bisuQuery.js
@@ -1,5 +1,40 @@
 /* eslint-disable */
 
+const makeNiceDecoratedCollection = collection => {
+	collection.each = callbackFn => {
+		collection.forEach((element, i) => {
+			const boundFn = callbackFn.bind(element);
+			boundFn(i);
+		});
+	};
+
+	// Adding on Fn prop
+	collection.on = (eventName, handlerFn) => {
+		collection.forEach(element => {
+			element.addEventListener(element, handlerFn);
+		});
+	};
+
+	// Adding css Fn prop
+	collection.css = (...cssArgs) => {
+		if (typeof cssArgs[0] === 'string') {
+			const [property, value] = cssArgs;
+
+			collection.forEach((element, i) => {
+				element.style[property] = value;
+			});
+		} else if (typeof cssArgs[0] === 'object') {
+			const cssProps = Object.entries(cssArgs[0]);
+
+			collection.forEach(element => {
+				cssProps.forEach(([property, value]) => {
+					element.style[property] = value;
+				});
+			});
+		}
+	};
+};
+
 const $ = (...args) => {
 	if (typeof args[0] === 'function') {
 		// document ready fn
@@ -9,45 +44,13 @@ const $ = (...args) => {
 		// Element is a selector
 		const selector = args[0];
 		const collection = document.querySelectorAll(selector);
-
-		collection.each = callbackFn => {
-			collection.forEach((element, i) => {
-				const boundFn = callbackFn.bind(element);
-				boundFn(i);
-			});
-		};
-
-		// Adding on Fn prop
-		collection.on = (eventName, handlerFn) => {
-			collection.forEach(element => {
-				element.addEventListener(element, handlerFn);
-			});
-		};
-
-		// Adding css Fn prop
-		collection.css = (...cssArgs) => {
-			if (typeof cssArgs[0] === 'string') {
-				const [property, value] = cssArgs;
-
-				collection.forEach((element, i) => {
-					element.style[property] = value;
-				});
-			} else if (typeof cssArgs[0] === 'object') {
-				const cssProps = Object.entries(cssArgs[0]);
-
-				collection.forEach(element => {
-					cssProps.forEach(([property, value]) => {
-						element.style[property] = value;
-					});
-				});
-			}
-		};
-
+		makeNiceDecoratedCollection(collection);
 		return collection;
-	} else if (
-		typeof args[0] === 'object' &&
-		args[0].constructor.name.startsWith('HTML')
-	) {
+	} else if (args[0] instanceof HTMLElement) {
 		console.log('We have an element!');
+
+		const collection = [args[0]];
+		makeNiceDecoratedCollection(collection);
+		return collection;
 	}
 };
