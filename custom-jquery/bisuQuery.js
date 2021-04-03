d368d2b28041ea4f246a80556af280c51d2aa1df
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-03 23:56:20 +0530

    Handling on and css for $

--- custom-jquery/bisuQuery.js
+++ custom-jquery/bisuQuery.js
@@ -1,43 +1,48 @@
 /* eslint-disable */
 
 const $ = (...args) => {
-
-	if(typeof args[0] === 'function') {
+	if (typeof args[0] === 'function') {
 		// document ready fn
-		const readyFn = args[0]
-		document.addEventListener('DOMContentLoaded', readyFn)
-	} else if(typeof args[0] === 'string') {
+		const readyFn = args[0];
+		document.addEventListener('DOMContentLoaded', readyFn);
+	} else if (typeof args[0] === 'string') {
 		// Element is a selector
-		const selector = args[0]
-		const collection =  document.querySelectorAll(selector)
+		const selector = args[0];
+		const collection = document.querySelectorAll(selector);
 
+		collection.each = callbackFn => {
+			collection.forEach((element, i) => {
+				const boundFn = callbackFn.bind(element);
+				boundFn(i);
+			});
+		};
+
+		// Adding on Fn prop
+		collection.on = (eventName, handlerFn) => {
+			collection.forEach(element => {
+				element.addEventListener(element, handlerFn);
+			});
+		};
+
+		// Adding css Fn prop
 		collection.css = (...cssArgs) => {
-
-			if(typeof cssArgs[0] === 'string') {
-				const [property, value] = cssArgs
+			if (typeof cssArgs[0] === 'string') {
+				const [property, value] = cssArgs;
 
 				collection.forEach((element, i) => {
-					element.style[property] = value
+					element.style[property] = value;
 				});
+			} else if (typeof cssArgs[0] === 'object') {
+				const cssProps = Object.entries(cssArgs[0]);
 
-			}else if(typeof cssArgs[0] === 'object') {
-
-					const cssProps = Object.entries(cssArgs[0])
-
-					collection.forEach(element => {
-						cssProps.forEach(([property, value]) => {
-								element.style[property] = value
-							});
-
-					})
-
-
-					};
-
+				collection.forEach(element => {
+					cssProps.forEach(([property, value]) => {
+						element.style[property] = value;
+					});
+				});
 			}
+		};
 
-			return collection
-
-
+		return collection;
 	}
-	}
+};
